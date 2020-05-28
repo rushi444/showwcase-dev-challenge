ac8c4b81267aec0c4f71f7dd1b042a0f6c64948c
rushi444 <rushi44@gmail.com>
2020-05-28 11:04:00 -0700

    initialize app and tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render } from 'enzyme';
+import { App } from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('App Component Renders', () => {
+  const wrapper = render(<App />);
+  expect(wrapper.length).toBe(1);
 });
